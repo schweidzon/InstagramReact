68754eea0d549713dfcf36b54c6ff9a2bf553e19
Schweidzon <dannielsch@gmail.com>
2022-11-17 15:53:02 -0300

    Componentização dos elementos

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import NavBar from "./NavBar"
+import Corpo from "./Corpo"
+
+export default function App() {
+    return (
+        <div class="content">
+            <div class="navbar">
+                <NavBar/>
+            </div>
+            <Corpo/>
+
+
+
+            <div class="fundo-mobile">
+                <ion-icon name="home"></ion-icon>
+                <ion-icon name="search-outline"></ion-icon>
+                <ion-icon name="add-circle-outline"></ion-icon>
+                <ion-icon name="heart-outline"></ion-icon>
+                <ion-icon name="person-outline"></ion-icon>
+            </div>
+        </div>)
+
+}
---
+++ src/components/Sugestoes.js
@@ -0,0 +1,70 @@
+export default function Sugestoes() {
+    return (
+        <div class="sugestoes">
+            <div class="titulo">
+                Sugestões para você
+                <div>Ver tudo</div>
+            </div>
+
+            <div class="sugestao">
+                <div class="usuario">
+                    <img src="assets/img/bad.vibes.memes.svg" alt="UsuarioImage"/>
+                    <div class="texto">
+                        <div class="nome">bad.vibes.memes</div>
+                        <div class="razao">Segue você</div>
+                    </div>
+                </div>
+
+                <div class="seguir">Seguir</div>
+            </div>
+
+            <div class="sugestao">
+                <div class="usuario">
+                    <img src="assets/img/chibirdart.svg" alt="UsuarioImage" />
+                    <div class="texto">
+                        <div class="nome">chibirdart</div>
+                        <div class="razao">Segue você</div>
+                    </div>
+                </div>
+
+                <div class="seguir">Seguir</div>
+            </div>
+
+            <div class="sugestao">
+                <div class="usuario">
+                    <img src="assets/img/razoesparaacreditar.svg" alt="UsuarioImage" />
+                    <div class="texto">
+                        <div class="nome">razoesparaacreditar</div>
+                        <div class="razao">Novo no Instagram</div>
+                    </div>
+                </div>
+
+                <div class="seguir">Seguir</div>
+            </div>
+
+            <div class="sugestao">
+                <div class="usuario">
+                    <img src="assets/img/adorable_animals.svg" alt="UsuarioImage" />
+                    <div class="texto">
+                        <div class="nome">adorable_animals</div>
+                        <div class="razao">Segue você</div>
+                    </div>
+                </div>
+
+                <div class="seguir">Seguir</div>
+            </div>
+
+            <div class="sugestao">
+                <div class="usuario">
+                    <img src="assets/img/smallcutecats.svg" alt="UsuarioImage" />
+                    <div class="texto"> 
+                        <div class="nome">smallcutecats</div>
+                        <div class="razao">Segue você</div>
+                    </div>
+                </div>
+
+                <div class="seguir">Seguir</div>
+            </div>
+        </div>
+    )
+}
