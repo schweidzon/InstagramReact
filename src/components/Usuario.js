6dae14e14b4e23e3d7a4d44f52a56b24ef9fb6db
Schweidzon <dannielsch@gmail.com>
2022-11-19 00:18:09 -0300

    Adicionado correção aut.

--- src/components/Usuario.js
+++ src/components/Usuario.js
@@ -1,14 +1,6 @@
-// export default function Usuario() {
-//     const user = [{ userImage: "assets/img/catanacomics.svg", userName: "catanacomics", name: "Catana" }]
-//     return (
-//         <div class="usuario" data-test="user">
-//             {user.map((u) => <User user = {u}/>)}
-
 import { useState } from "react"
 
-//         </div>
-//     )
-// }
+
 
 export default function Usuario(props) {
 
@@ -19,8 +11,8 @@ export default function Usuario(props) {
         <div className="usuario" data-test="user">
             <img src={!userImage ? props.userImage : userImage} onClick={() => setUserImage(prompt('Digite um nome de usuário'))} alt="userImage" data-test="profile-image" />
             <div className="texto">
-                <strong data-test="name">{props.userName}</strong>
-                <span>
+                <strong >{props.userName}</strong>
+                <span data-test="name">
                     {!user ? props.name : user}
                     <ion-icon onClick={() => setUser(prompt("Digite um nome de usuário"))} name="pencil" data-test="edit-name"></ion-icon>
                 </span>
@@ -32,20 +24,4 @@ export default function Usuario(props) {
 }
 
 
-
-// function User(props) {
-//     return (
-//         <div>
-//             <img src={props.user.userImage} alt="userImage" data-test="profile-image" />
-//             <div class="texto">
-//                 <strong data-test="name">{props.user.userName}</strong>
-//                 <span>
-//                     {props.user.name}
-//                     <ion-icon name="pencil" data-test="edit-name"></ion-icon>
-//                 </span>
-//             </div>
-//         </div>
-//     )
-
-// }
 
